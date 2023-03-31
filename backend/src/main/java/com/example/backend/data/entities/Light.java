5770caa7a78508c6390b2b9df3262b05caad44fa
minhhoanhh <19521982@gm.uit.edu.vn>
2023-04-01 01:59:14 +0700

    edit entities

--- backend/src/main/java/com/example/backend/data/entities/Light.java
+++ backend/src/main/java/com/example/backend/data/entities/Light.java
@@ -4,18 +4,25 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.*;
 
 @Entity
-@Table(name = "temperature_tbl")
+@Table(name = "light_tbl")
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Light {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
     @Column(name = "light")
     private String light;
 
